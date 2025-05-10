4ad913671901ac8efc905bd19efb1a4263483dec
Madeira <ederlissemedeiros@gmail.com>
2025-05-10 12:25:42 -0300

    Implemented Seach Function
    Possível procurar livros no banco de dados através dos atributos adicionados

--- CRUD Algoritmos.py
+++ CRUD Algoritmos.py
@@ -4,10 +4,11 @@ import json
 # Verify if archive exist
 def arquivoexiste(nome):
     try:
-        with open(nome, 'r', encoding='utf-8') as arquivo:
-            json.load(arquivo)
+        with open(nome, 'r+', encoding='utf-8') as arquivo:
+            if bool(arquivo.read()) == False: # Checks if .json is empty
+                json.dump(obj = [], fp=arquivo, ensure_ascii=False, indent=2)
 
-    except FileNotFoundError:
+    except (FileNotFoundError):
         return False
 
     else:
@@ -17,10 +18,9 @@ def arquivoexiste(nome):
 def criararquivo(nome):
     try:
         with open(nome, 'w', encoding='utf-8') as arquivo:
-            json.dump(obj = [], fp=arquivo, ensure_ascii=False, indent=2) # Faz o dump do json com uma lista.
+            json.dump(obj = [], fp=arquivo, ensure_ascii=False, indent=2) # dump json with a list.
 
-    except Exception as erro:
-        print(erro)
+    except:
         print('Falha na criação do arquivo')
 
 # Default menu print.
@@ -35,62 +35,60 @@ def menu(opcoes):
 def adicionar(livro):
     verificacao_livro = False
 
+    # Verify if book has already been added
     with open(arquivo, 'r+', encoding='utf-8') as arquivo_json:
-        dados = json.load(arquivo_json)
-        for dicionario in dados: 
+        loaded = json.load(arquivo_json)
+        for dicionario in loaded: 
             if str(livro).upper().replace(' ', '') == str(dicionario).upper().replace(' ', ''):
-                verificacao_livro = True
+                verificacao_livro = True 
 
     if verificacao_livro:
         return "*Livro já adicionado*"
 
     else:
 
-        lista_livros.append(livro)
+        # Dump list loaded with old .json, updated with new book
+        lista_livros.append(livro.copy())
         with open(arquivo, 'w+', encoding='utf-8') as arquivo_json:
             json.dump(lista_livros, fp=arquivo_json, ensure_ascii=False, indent=2)
             return "Livro adicionado."
 
 # def function visualize book.
-def visualizar():
+def visualizar(data, start=0):
     indice = 1
 
-    with open(arquivo, 'r', encoding='utf-8') as txt:
-        for linha in txt.readlines():
-            print(f'{indice}. {linha}')
-            indice += 1
+    # Show archieve formated
+    for dicionario in data[start:]:
+        print(f'{indice}. {dicionario}'
+                .replace('{', '').replace('}', '')
+                .replace("'", '').replace(', ', ' | '))
+        indice += 1
 
 # def function update book.
 def atualizar(indice_livro, atributo, novo_atributo):
-    with open(arquivo, 'r+', encoding='utf-8') as txt:
-        foi_atualizado = False
-        indice = 1
+    lista_livros[indice_livro - 1][atributo] = novo_atributo # Update book list with parameters
 
-        for linha in txt.readlines(): # rewrites txt
-
-            if indice_livro == indice: # Checks if line corresponds to book index
-                print(linha)
-                print(atributo)
-                print(novo_atributo)
-                txt.seek(0, 0)
-                txt.write(linha.replace(atributo, novo_atributo))
-
-                foi_atualizado = True
-
-            else:
-                txt.seek(0, 0)
-                txt.write(linha)
-                print(linha)
-            
-            indice += 1
-
-        if foi_atualizado:
-            return "Livro atualizado."
-        return "*Livro para atualização não encontrado*"
+    # Dump json with new book list
+    with open(arquivo, 'w+', encoding='utf-8') as arquivo_json:
+        json.dump(lista_livros, fp=arquivo_json, ensure_ascii=False, indent=2)
+        return "Livro atualizado."
 
 # def function search book.
-def buscar():
-    ''
+def buscar(atributo, procurado):
+    achados = []
+
+    with open(arquivo, 'r', encoding='utf-8') as arquivo_json:
+        loaded = json.load(arquivo_json)
+
+        if atributo not in livro.keys(): # Checks if atribute exists
+            return False
+        
+        for dicionario in loaded: # Search in dictionarys for the atribute
+            if dicionario[atributo].upper() == procurado.upper():
+                achados.append(dicionario) # If founded, append the dict to found list.
+        
+    return achados
+                
 
 # def function delete book.
 def deletar():
@@ -108,23 +106,24 @@ livro = {
     'NOME': '',
     'AUTOR': '',
     'ASSUNTO': '',
-    'NÚMERO DE PÁGINAS': '',
+    'NUMERO DE PAGINAS': '',
     'EDITORA': '',
     'ISBN': ''
 }
 
+# Loads .json to list
 with open(arquivo, 'r+', encoding='utf-8') as arquivo_json:
     lista_livros = json.load(arquivo_json)
 
 # Interation menu.
 while True:
-    print(f'{"BIBLIOTECA":^150}\n{"-"*150}')
+    print(f'\n{"BIBLIOTECA":^150}\n{"-"*150}')
     menu(opcoes)
     print()
 
     # Verify choice type raising error in except ValueError/TypeError
     try:
-        opcao = int(input(" - Opção desejada: "))
+        opcao = int(input(" - Opção desejada [-1 p/ sair]: "))
         print()
 
     except (ValueError, TypeError):
@@ -138,7 +137,7 @@ while True:
             print(f'{"<Adicionar Livro>":^150}\n')
 
             for atributo in livro.keys():
-                livro[atributo] = input(f" - {atributo}: ") # Add each atribute to the dictionary
+                livro[atributo] = input(f" - {atributo}: ").strip() # Add each atribute to the dictionary
 
             print()
             print(adicionar(livro))
@@ -146,19 +145,32 @@ while True:
         # Execute visualize books function
         elif opcao == 2:
             print(f'{"<Livros>":^150}\n')
-            visualizar() 
+            with open(arquivo, 'r', encoding='utf-8') as arquivo_json:
+                loaded = json.load(arquivo_json)
+                visualizar(loaded)
 
         # Execute update book function
         elif opcao == 3:
             print(f'{"<Atualizar Livro>":^150}\n')
-            visualizar()
+            with open(arquivo, 'r', encoding='utf-8') as arquivo_json:
+                loaded = json.load(arquivo_json)
+                visualizar(loaded) # Visualize books function
+            print()
 
             # Verify index choice type raising error in except ValueError/TypeError
             try:
                 item = int(input('- Livro para atualização [índice]: '))
+                with open(arquivo, 'r', encoding='utf-8') as arquivo_json:
+                    loaded = json.load(arquivo_json)
+
+                if item not in range(1, len(loaded) + 1): # Verify if index is in the json range.
+                    raise IndexError
 
             except (ValueError, TypeError):
                 print("\n*Digite apenas números*\n")
+
+            except(IndexError):
+                print("\n*Índice do livro desconhecido*\n")
             
             else:
                 atributo = input('- Atributo para atualização: ').upper().strip() # Choose attribute to update
@@ -173,12 +185,30 @@ while True:
         # Execute search book function
         elif opcao == 4:
             print(f'{"<Buscar Livro>":^150}\n')
-            buscar()
+            print(f"ATRIBUTOS: {list(livro.keys())}\n".replace("'", ""))
+
+            nome_atributo = input('- Atributo para busca: ').strip().upper()
+            atributo_procurado = input(f'- Pesquisa ({nome_atributo}): ').strip()
+
+            resultado = buscar(nome_atributo, atributo_procurado)
+
+            if bool(resultado) == False:
+                print("*Não foram achados resultados para pesquisa*")
+            
+            else:
+                print(f"\nForam achados {len(resultado)} resultados para sua pesquisa: \n")
+                visualizar(resultado)
+
 
         # Execute delete book function
         elif opcao == 5:
             print(f'{"<Deletar Livro>":^150}\n')
             deletar()
+
+        # End program option
+        elif opcao == -1:
+            print(f'{"<ENCERRANDO SISTEMA>":^150}\n')
+            break
 
         # User's choice out of the choice's range.
         else:
